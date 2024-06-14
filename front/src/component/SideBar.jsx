e39e452e21fd6d24ea60528475178fdbf946036d
kamakura0523 <suieikama0523@gmail.com>
2024-06-14 19:21:34 +0900

    SideBarの修正

--- front/src/component/SideBar.jsx
+++ front/src/component/SideBar.jsx
@@ -1,56 +1,94 @@
 import {
-    Box,
-    Drawer,
-    Link,
-    List,
-    ListItem,
-    ListItemButton,
-    ListItemIcon,
-    Toolbar,
+  Box,
+  Drawer,
+  Link,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  Toolbar,
 } from "@mui/material";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 import React from "react";
 import { subColor } from "@/global/global";
+import CreateIcon from "@mui/icons-material/Create";
+import HomeIcon from "@mui/icons-material/Home";
+import PersonIcon from "@mui/icons-material/Person";
+import InfoIcon from "@mui/icons-material/Info";
+import Button from "@mui/material/Button";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const drawerWidth = 240;
+const name = ["Home", "Create", "Profile", "About"];
 
 const SideBar = () => {
-    return (
-
-        <Drawer
-            variant="permanent"
-            sx={{
-                width: drawerWidth,
-                flexShrink: 0,
-                [`& .MuiDrawer-paper`]: {
-                    width: drawerWidth,
-                    boxSizing: "border-box",
-                },
-            }}
-        >
-            <Toolbar />
-            <Box sx={{
-                overflow: "auto", backgloudColor: subColor,
-
-            }}>
-                <List>
-                    {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-                        <ListItem key={text} disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                </ListItemIcon>
-                                <Link href="/page1" underline="none" color="inherit">
-                                    Page1
-                                </Link>
-                            </ListItemButton>
-                        </ListItem>
-                    ))}
-                </List>
-            </Box>
-        </Drawer>
-    );
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        [`& .MuiDrawer-paper`]: {
+          width: drawerWidth,
+          boxSizing: "border-box",
+        },
+      }}
+    >
+      <Toolbar />
+      <Box
+        sx={{
+          overflow: "auto",
+        }}
+      >
+        <List>
+          {[<HomeIcon />, <CreateIcon />, <PersonIcon />, <InfoIcon />].map(
+            (text, index) => (
+              <ListItem key={text} style={{ marginTop: "5px" }}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <div>
+                      <div key={index}>{text}</div>
+                    </div>
+                    <Link
+                      href={name[index]}
+                      underline="none"
+                      color="inherit"
+                      style={{ marginLeft: "50px" }}
+                    >
+                      {name[index]}
+                    </Link>
+                  </ListItemIcon>
+                </ListItemButton>
+              </ListItem>
+            )
+          )}
+        </List>
+      </Box>
+      <Box
+        sx={{
+          overflow: "auto",
+        }}
+      >
+        <List style={{ marginTop: "450px" }}>
+          <ListItem>
+            <ListItemButton>
+              <LogoutIcon style={{ color: "inherit" }} />
+              <Link
+                underline="none"
+                style={{
+                  marginLeft: "50px",
+                  color: "inherit",
+                }}
+              >
+                Logout
+              </Link>
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
+    </Drawer>
+  );
 };
 
 export default SideBar;
