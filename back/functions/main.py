3b1cc13dd5e3263e68be7e8a6d65496658d0a3ad
tt1125 <tsuru.takeshi275@gmail.com>
2024-06-16 04:48:08 +0900

    init

--- back/functions/main.py
+++ back/functions/main.py
@@ -3,12 +3,17 @@
 # Deploy with `firebase deploy`
 
 from firebase_functions import https_fn
-from firebase_admin import initialize_app
+from firebase_admin import initialize_app , storage , firestore
+
 
 initialize_app()
 
 
-@https_fn.on_request()
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
+@https_fn.on_call()
+def create_img(req: https_fn.CallableRequest):
+    # file_id = req.data["fileId"]
+    file_id = "bmo.png"
+    img_file  = storage.bucket().file(f"org-imgs/{file_id}")
+    print(img_file)
 
-    return https_fn.Response("")
+    return https_fn.Response("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
