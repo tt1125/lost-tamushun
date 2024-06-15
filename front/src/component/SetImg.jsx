e86598412564eb8ad95648e1a2976d6352740829
kamakura0523 <suieikama0523@gmail.com>
2024-06-15 21:21:54 +0900

    Homeのimg取得とcreate画面の作成

---
+++ front/src/component/SetImg.jsx
@@ -0,0 +1,40 @@
+import { Image, UploadFile } from "@mui/icons-material";
+import { Button, Typography } from "@mui/material";
+
+export default function SetImg(file, disabled, handleFileChange) {
+  return (
+    <div className="w-full">
+      <Button
+        variant="outlined"
+        component="label"
+        // disabled={disabled}
+        sx={{
+          minHeight: "200px",
+          width: "700px",
+          borderRadius: "20px",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Typography
+          variant="body1"
+          align="center"
+          className="whitespace-pre-wrap overflow-y-scroll"
+          sx={{ fontSize: "12px", fontWeight: "bold", scrollbarWidth: "none" }}
+          component="div"
+        >
+          <div>
+            <Image sx={{ fontSize: "100px" }} />
+            <div>画像ファイルを選択</div>
+          </div>
+        </Typography>
+        <input
+          type="file"
+          hidden
+          multiple={false}
+          onChange={handleFileChange}
+        />
+      </Button>
+    </div>
+  );
+}
