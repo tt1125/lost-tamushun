cb45400e239242e9f328ee6c557fab205114fb96
subarushu <717104tn.family.0411@gmail.com>
2024-06-15 21:21:41 +0900

    画像生成部分変更

--- back/library/translate_ja2en.py
+++ back/library/translate_ja2en.py
@@ -1,5 +1,3 @@
-#無駄に技術使ってAPIで実装してみるかも。
-
 #pip install translate
 
 from translate import Translator
@@ -9,4 +7,4 @@ def translate_2en_text(text):#オフライン対応
     return translater.translate(text)
 
 #テスト
-print(translate_2en_text('ディズニー風の男性'))
+# print(translate_2en_text('AI とコグニティブ サービスを使用して、顧客の興味や行動に基づいた、クラウドに接続されたモバイル エクスペリエンスを構築できます。'))
