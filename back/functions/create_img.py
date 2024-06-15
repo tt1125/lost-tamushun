4b74d52d54d5ec1d7a5172a164286716f1845ac4
tt1125 <tsurutsuru275@gmail.com>
2024-06-16 01:13:19 +0900

    firebase functions

--- back/functions/create_img.py
+++ back/functions/create_img.py
@@ -11,35 +11,30 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # 環境変数の取得
-search_service_endpoint = os.getenv('SEARCH_SERVICE_ENDPOINT')
-search_service_key = os.getenv('SEARCH_SERVICE_KEY')
-search_index_name = os.getenv('SEARCH_INDEX_NAME')
-openai_api_key = os.getenv('OPENAI_API_KEY')
-storage_account_name = os.getenv('AZURE_STORAGE_ACCOUNT_NAME')
+
+search_service_endpoint = os.getenv("SEARCH_SERVICE_ENDPOINT")
+search_service_key = os.getenv("SEARCH_SERVICE_KEY")
+search_index_name = os.getenv("SEARCH_INDEX_NAME")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+storage_account_name = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
 
 bp = func.Blueprint()
 
+
 @bp.route(route="default_template")
 def default_template(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Python HTTP trigger function processed a request.")
 
     try:
         if not openai_api_key:
-            raise ValueError('OpenAI API key is required.')
+            raise ValueError("OpenAI API key is required.")
 
         # BlobのURL取得
         blob_url = f"https://{storage_account_name}.blob.core.windows.net/imgs/IMG_4415_Original.jpg"
         print(blob_url)
-        
 
     except Exception as e:
         logging.error(e)
-        return func.HttpResponse(
-            "Error occurred: " + str(e),
-            status_code=500
-        )
+        return func.HttpResponse("Error occurred: " + str(e), status_code=500)
 
-    return func.HttpResponse(
-        "Processing completed successfully.",
-        status_code=200
-    )
+    return func.HttpResponse("Processing completed successfully.", status_code=200)
--- back/functions/http_blueprint.py
+++ back/functions/http_blueprint.py
@@ -11,29 +11,29 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # 環境変数の取得
-search_service_endpoint = os.getenv('SEARCH_SERVICE_ENDPOINT')
-search_service_key = os.getenv('SEARCH_SERVICE_KEY')
-search_index_name = os.getenv('SEARCH_INDEX_NAME')
-openai_api_key = os.getenv('OPENAI_API_KEY')
-storage_account_name = os.getenv('AZURE_STORAGE_ACCOUNT_NAME')
+search_service_endpoint = os.getenv("SEARCH_SERVICE_ENDPOINT")
+search_service_key = os.getenv("SEARCH_SERVICE_KEY")
+search_index_name = os.getenv("SEARCH_INDEX_NAME")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+storage_account_name = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
 
 # OpenAIのAPIキー設定
 client = OpenAI(api_key=openai_api_key)
 
 bp = func.Blueprint()
 
+
 @bp.route(route="default_template")
 def default_template(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Python HTTP trigger function processed a request.")
 
     try:
         if not openai_api_key:
-            raise ValueError('OpenAI API key is required.')
+            raise ValueError("OpenAI API key is required.")
 
         # BlobのURL取得
         blob_url = f"https://{storage_account_name}.blob.core.windows.net/imgs/IMG_4415_Original.jpg"
         print(blob_url)
-        
 
         # 1. 画像を説明
         # ChatGPTを使用して画像の説明を生成
@@ -41,36 +41,34 @@ def default_template(req: func.HttpRequest) -> func.HttpResponse:
             model="gpt-4o-2024-05-13",
             messages=[
                 {
-                "role": "user",
-                "content": [
-                    {"type": "text", "text": "What’s in this image?"},
-                    {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": blob_url,
-                        "detail": "low"
-                    },
-                    },
-                ],
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": "What’s in this image?"},
+                        {
+                            "type": "image_url",
+                            "image_url": {"url": blob_url, "detail": "low"},
+                        },
+                    ],
                 }
             ],
             max_tokens=300,
-            )
+        )
         imgDescription = response.choices[0].message.content
-        logging.info('Image Description: %s', imgDescription)
+        logging.info("Image Description: %s", imgDescription)
 
         # 2. 埋め込みの生成
-        embedding_response = client.embeddings.create(input=imgDescription,
-        model="text-embedding-ada-002")
+        embedding_response = client.embeddings.create(
+            input=imgDescription, model="text-embedding-ada-002"
+        )
         embeddings = embedding_response.data[0].embedding
 
-        logging.info('Embeddings: %s', embeddings)
+        logging.info("Embeddings: %s", embeddings)
 
         # 3. Azure AI Searchにベクトルデータを登録
         search_client = SearchClient(
             endpoint=search_service_endpoint,
             index_name=search_index_name,
-            credential=AzureKeyCredential(search_service_key)
+            credential=AzureKeyCredential(search_service_key),
         )
 
         # ランダムなIDを生成
@@ -84,23 +82,17 @@ def default_template(req: func.HttpRequest) -> func.HttpResponse:
                 "metaData": {
                     "title": f"{random_index}",
                     "url": blob_url,
-                }
+                },
             }
         ]
 
-        logging.info('Documents to Index: %s', documents_to_index)
+        logging.info("Documents to Index: %s", documents_to_index)
 
         res = search_client.upload_documents(documents=documents_to_index)
-        logging.info('Upload Documents Result: %s', res)
+        logging.info("Upload Documents Result: %s", res)
 
     except Exception as e:
         logging.error(e)
-        return func.HttpResponse(
-            "Error occurred: " + str(e),
-            status_code=500
-        )
+        return func.HttpResponse("Error occurred: " + str(e), status_code=500)
 
-    return func.HttpResponse(
-        "Processing completed successfully.",
-        status_code=200
-    )
+    return func.HttpResponse("Processing completed successfully.", status_code=200)
