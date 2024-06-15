f3e2476935a9230ef969a3008e70d2ba861c585b
subarushu <717104tn.family.0411@gmail.com>
2024-06-15 21:32:10 +0900

    modify about

--- front/src/app/about/page.jsx
+++ front/src/app/about/page.jsx
@@ -3,10 +3,29 @@ export default function About() {
         <div style={{ height: "100%", backgroundColor: "#010101", display: "flex", justifyContent: "center", alignItems: "center" }}>
             <div style={{ color: "#fff" }}>
                 <h1>About</h1>
-                <p>このアプリは画像生成AIを使っています</p>
-                <p>Reactは、Facebookが開発したJavaScriptライブラリで、</p>
-                <p>コンポーネント指向のUIライブラリです。</p>
+                <p>画像をパラレルワールドに変換するアプリです。</p>
+                <p>変換した画像はみんなで共有できます。
+                    <br></br>共有した画像は、他のユーザーが見ることができます。
+                    <br></br>変換した画像を元の画像と見比べてみましょう！
+                </p>
+                <p>このアプリに使用している技術は、以下の通りです。
+                    <li>React</li>
+                    <li>Flask</li>
+                    <li>Novel AI API</li>
+                    <li>functons</li>
+                    <li>store & blob storage</li>
+                </p>
+                <p>Reactは、Facebookが開発したJavaScriptライブラリで、コンポーネント指向のUIライブラリです。</p>
+                <p>Flaskは、PythonのWebアプリケーションフレームワークです。</p>
+                <p>Novel AI APIは、画像生成AIを提供しているAPIです。</p>
+                <p>functionsは、サーバーレスアーキテクチャを提供しているサービスです。</p>
+                <p>store & blob storageは、画像を保存するためのストレージです。</p>
+
+
+
             </div>
+
         </div>
+
     )
 }
