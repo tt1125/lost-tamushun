6f1d3cb3f6971c8f2dc2b07a5e67a57ada7553be
smaru1111 <marusou20@gmail.com>
2024-06-16 04:37:32 +0900

    modify func

--- back/functions/main.py
+++ back/functions/main.py
@@ -1,6 +1,17 @@
+import json
+import logging
+import os
+from google.cloud import functions_v1
+from dotenv import load_dotenv
+from app.imgSearch import img_search
 
-import functions_framework
+load_dotenv()
 
-@functions_framework.http
-def hello_get(request):
-    return "Hello World!"
+# 環境変数の取得
+search_service_endpoint = os.getenv('SEARCH_SERVICE_ENDPOINT')
+search_service_key = os.getenv('SEARCH_SERVICE_KEY')
+search_index_name = os.getenv('SEARCH_INDEX_NAME')
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+def img_search_entry_point(request):
+    return img_search(request, search_service_endpoint, search_service_key, search_index_name, openai_api_key)
