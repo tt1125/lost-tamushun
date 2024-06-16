f6d8ff03b6b78f7c42305ecfabff2449fd34fca7
kamakura0523 <suieikama0523@gmail.com>
2024-06-16 11:24:40 +0900

    様々な修正

--- front/src/app/page.jsx
+++ front/src/app/page.jsx
@@ -37,27 +37,31 @@ export default function Home() {
 
 const ImageList = ({ data, onSelect }) => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: "70px",
-        overflow: "hidden",
-      }}
-    >
-      {data.map((image, index) => (
-        <div key={index}>
-          <Button
-            variant="outlined"
-            style={{ width: "250px", height: "250px" }}
-            onClick={() => onSelect(image)}
-          >
-            <img src={image.urls.small} alt={image.description} />
-          </Button>
-        </div>
-      ))}
+    <div>
+      <h1 style={{ marginLeft: "520px" }}>Home</h1>
+      <div style={{ margin: "15px" }}></div>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "70px",
+          overflow: "hidden",
+        }}
+      >
+        {data.map((image, index) => (
+          <div key={index}>
+            <Button
+              variant="outlined"
+              style={{ width: "250px", height: "250px" }}
+              onClick={() => onSelect(image)}
+            >
+              <img src={image.urls.genImgUrl} />
+            </Button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
@@ -65,14 +69,63 @@ const ImageList = ({ data, onSelect }) => {
 const ImageDetail = ({ image, onBack }) => {
   return (
     <div style={{ textAlign: "center" }}>
-      <Button onClick={onBack}>Back</Button>
-      <div>
-        <img
-          src={image.urls.full}
-          alt={image.description}
-          style={{ maxWidth: "100%" }}
-        />
-        <p>{image.description}</p>
+      <h1
+        style={{
+          marginTop: "30px",
+          backgroundColor: "#edf6f9",
+          color: "black",
+          width: "200px",
+          textAlign: "center",
+          marginTop: "50px",
+          marginLeft: "475px",
+        }}
+      >
+        画像詳細
+      </h1>
+      <div
+        style={{
+          marginTop: "150px",
+          display: "flex",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          gap: "100px",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <img
+            src={image.urls.orgImgUrl}
+            style={{
+              maxWidth: "100%",
+              width: "400px",
+              height: "400px",
+            }}
+          />
+          <p>オリジナル画像</p>
+        </div>
+        <div>
+          <img
+            src={image.urls.genImgUrl}
+            style={{
+              maxWidth: "100%",
+              width: "400px",
+              height: "400px",
+            }}
+          />
+          <p>生成画像</p>
+        </div>
+        <Button
+          onClick={onBack}
+          style={{
+            textAlign: "center",
+            width: "100px",
+            height: "50px",
+            marginLeft: "200px",
+            marginRight: "200px",
+          }}
+        >
+          Back
+        </Button>
       </div>
     </div>
   );
